433de9f876dcc0c03762f2cce236e3779547976a
R Sriram <rinnegan.sr@gmail.com>
2024-02-26 07:27:03 +0000

    dotenv

--- myApp.js
+++ myApp.js
@@ -1,9 +1,22 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
-
-
-
+// app.get("/",(req,res)=>{
+//     res.sendFile(__dirname + '/views/index.html');
+// });
+app.get("/json",(req,res)=>{
+    //res.json({"message": "Hello json"});
+    if (process.env.MESSAGE_STYLE === "allCaps") {
+        res.json({"message": "HELLO JSON"});
+      } else {
+        res.json({"message": "Hello json"});
+      }
+}
+);
+app.use(
+    "/public",express.static(__dirname + '/public')
+);
 
 
 
